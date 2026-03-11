470849e22a719dfca2f6391f474982931ae14e44
Brianarabe <bc.alexanderbrian.arabe@cvsu.edu.ph>
2026-03-11 23:34:18 +0800

    Add files via upload

---
+++ Django capstone project/project/app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import HomePageView,AboutPageView,ListingsPageView,LoginPageView,RegisterPageView,View_propertyPageView,SearchPageView,ContractPageView,BasePageView
+
+urlpatterns = {
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('', ListingsPageView.as_view(), name='listing'),
+    path('', ContractPageView.as_view(), name='contact'),
+    path('', LoginPageView.as_view(), name='login'),
+    path('', View_propertyPageView.as_view(), name='view_property'),
+    path('', RegisterPageView.as_view(), name='register'),
+    path('', SearchPageView.as_view(), name='search'),
+}
